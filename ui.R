c5eed204b727b20c69e2014ff755c929621acb5d
blei139 <blei139@gmail.com>
2016-09-21 07:25:10 -0700

    Update ui.R according to students' comments

--- ui.R
+++ ui.R
@@ -16,14 +16,14 @@ shinyUI(fluidPage(
   sidebarLayout(
   
     sidebarPanel(
-      textInput("caption", "Enter the stock symbol", ""),
+      textInput("caption", "Enter the stock or index symbol", ""),
       selectInput("dataset", "Select a choice", 
-                  choices = c("Stock Quote", "Stock Analysis Summary", "Stock Kalman Filter Prediction", 
-                              "Stock Forecast", "Stock Moving Average Convergence Divergence",
-                              "Stock Simple Moving Averages",
+                  choices = c("Stock Quote",  
+                              "Stock Moving Average Convergence Divergence", "Stock Simple Moving Averages",
 					"Stock On Balance Volume", "Stock Accumulation Distribution Line",
 					"Stock Average Directional Index", "Stock Aroon Indicator",
-					"Stock Relative Strength Index", "Stock Stochastic Oscillator"
+					"Stock Relative Strength Index", "Stock Stochastic Oscillator",
+					"Stock Kalman Filter Prediction", "Stock Forecast", "Stock Analysis Summary"
                               )),
       sliderInput("numDays", "Please Use Slider to Generate the Number of Days for the Plot", 
                   value = 65, min = 36, max = 2600, step = 1),
